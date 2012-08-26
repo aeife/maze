7c3aead3cad2c2f255a055c1a1cb5263955d0f59
Andre Eife <andre.eife@gmail.com>
2012-08-26 15:24:03 +0200

    bugfixes and some improvements like variable view size

--- app.js
+++ app.js
@@ -4,32 +4,32 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+    , routes = require('./routes')
+    , http = require('http')
+    , path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 
 var server = app.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
@@ -37,38 +37,38 @@ var io = require('socket.io').listen(server);
 var players = [];
 
 io.sockets.on('connection', function (socket) {
-  console.log("connected");
+    console.log("connected");
 
-  //generate unique id
-  var idNr = Math.round(new Date()*Math.random());
-  
-  console.log("idNr: " + idNr);
-  var connectionData = {idNr: idNr, currentPlayers: players};
+    //generate unique id
+    var idNr = Math.round(new Date()*Math.random());
+    
+    console.log("idNr: " + idNr);
+    var connectionData = {idNr: idNr, currentPlayers: players};
 
-  socket.emit('successfullyConnected', connectionData);
-  socket.broadcast.emit('newPlayer', idNr);
+    socket.emit('successfullyConnected', connectionData);
+    socket.broadcast.emit('newPlayer', idNr);
 
-  players.push({x: 50, y: 50, idNr: idNr});
+    players.push({x: 50, y: 50, idNr: idNr});
 
-  socket.on("move", function(data){
-    console.log("move");
-    console.log(data.x);
+    socket.on("move", function(data){
+        console.log("move");
+        console.log(data.x);
 
-    var client;
-    for (var i=0; i<players.length; i++){
-      if (players[i].idNr === data.idNr) {
-        client = players[i];
-      }
-    }
-    client.x = data.x;
-    client.y = data.y;
+        var client;
+        for (var i=0; i<players.length; i++){
+            if (players[i].idNr === data.idNr) {
+                client = players[i];
+            }
+        }
+        client.x = data.x;
+        client.y = data.y;
 
 
-    socket.broadcast.emit('newPosition', data);
-  });
+        socket.broadcast.emit('newPosition', data);
+    });
 
-  socket.on("disconnect", function(){
-    console.log("disconnected client!");
-  });
+    socket.on("disconnect", function(){
+        console.log("disconnected client!");
+    });
 
 });
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,208 +12,256 @@ wall.src = "images/wall.png";
 var floor = new Image();
 floor.src = "images/floor.png";
 
-var player = {x: 1, y: 1};
-var players = [];
+
 
 var levelWidth = 100;
 var levelHeight = 100;
+var viewWidth = 5;
+var offset = Math.floor(viewWidth/2);
 var tileWidth = 100;
+var spawn = {x:3, y:3};
+
+var players = [];
+
+var player = {x: spawn.x, y: spawn.y};
+
 
 var url = "http://localhost:3000";
 var socket = io.connect(url);
 
 
+
+
 document.onkeydown = function(e) {
-	switch(e.keyCode) {
-		case 87:
-			//w
-			moveUp(10);
-			break;
-		case 65:
-			//a
-			moveLeft(10);
-			break;
-		case 83:
-			//s
-			moveDown(10);
-			break;
-		case 68:
-			//d
-			moveRight(10);
-			break;
-	};
+    switch(e.keyCode) {
+        case 87:
+            //w
+            moveUp(10);
+            break;
+        case 65:
+            //a
+            moveLeft(10);
+            break;
+        case 83:
+            //s
+            moveDown(10);
+            break;
+        case 68:
+            //d
+            moveRight(10);
+            break;
+    };
 };
 
 //----------------------------SOCKETS-----------------
 
 //starting position
 img.onload = function(){
-	ctx.drawImage(img, player.x,player.y,tileWidth,tileWidth);
-	console.log(img.width);
+    ctx.drawImage(img, player.x,player.y,tileWidth,tileWidth);
+    console.log(img.width);
 
-	level = generateLevel();
-	drawView();
+    level = generateLevel();
+    
+    level[spawn.x][spawn.y].players++;
 
 };
 
 socket.on('successfullyConnected', function (data) {
-	console.log("successfully connected");
-	console.log(data);
-	player.idNr = data.idNr;
-	players = data.currentPlayers;
-	console.log(players);
-	for (var i=0; i<players.length; i++){
-		moveTo(players[i].idNr, players[i].x, players[i].y);
-	}
+    // own player connected and receiving data like all current players
+    console.log("successfully connected");
+    console.log(data);
+    player.idNr = data.idNr;
+    players = data.currentPlayers;
+    console.log(players);
+    for (var i=0; i<players.length; i++){
+        spawnClient(players[i].x, players[i].y);
+    }
+    drawView();
 });
 
 socket.on('newPlayer', function (idNr) {
-	console.log("got new player");
-	//console.log(idNr);
-	players.push({x: 1, y: 1, idNr: idNr});
-	moveTo(idNr,1,1);
-	console.log(players);
+    // new player connected, spawn player
+    console.log("got new player");
+    //console.log(idNr);
+    players.push({x: spawn.x, y: spawn.y, idNr: idNr});
+    spawnClient(spawn.x, spawn.y);
+    console.log(players);
 });
 
 socket.on('newPosition', function (data) {
-	console.log("got new position");
-	moveTo(data.idNr, data.x, data.y);
+    // other player moved
+    console.log("got new position");
+    moveTo(data.idNr, data.x, data.y);
 });
 
 
 //----------------------------FUNCTIONS-----------------
 
 function moveUp(speed){
-	level[player.x][player.y].player = false;
-	player.y--;
-	level[player.x][player.y].player = true;
+    if (level[player.x][player.y-1].background === "wall")
+        return;
 
-	drawView();
+    level[player.x][player.y].players--;
+    player.y--;
+    level[player.x][player.y].players++;
 
-	emitNewPosition();
+    drawView();
+
+    emitNewPosition();
 }
 
 function moveLeft(speed){
-	level[player.x][player.y].player = false;
-	player.x--;
-	level[player.x][player.y].player = true;
+    if (level[player.x-1][player.y].background === "wall")
+        return;
 
-	drawView();
+    level[player.x][player.y].players--;
+    player.x--;
+    level[player.x][player.y].players++;
 
-	emitNewPosition();
+    drawView();
+
+    emitNewPosition();
 }
 
 function moveDown(speed){
-	console.log("!!!!!");
-	console.log(level[1][1]);
-	level[player.x][player.y].player = false;
-	player.y++;
-	level[player.x][player.y].player = true;
+    if (level[player.x][player.y+1].background === "wall")
+        return;
 
-	drawView();
+    level[player.x][player.y].players--;
+    player.y++;
+    level[player.x][player.y].players++;
 
-	emitNewPosition();
+    drawView();
+
+    emitNewPosition();
 }
 
 function moveRight(speed){
-	level[player.x][player.y].player = false;
-	player.x++;
-	level[player.x][player.y].player = true;
+    if (level[player.x+1][player.y].background === "wall")
+        return;
 
-	drawView();
+    level[player.x][player.y].players--;
+    player.x++;
+    level[player.x][player.y].players++;
 
-	emitNewPosition();
+    drawView();
+
+    emitNewPosition();
+}
+
+function spawnClient(x,y){
+    level[x][y].players++;
+    if (isVisible(x,y)) {
+        drawClient(x,y);
+    }
+}
+
+function drawClient(x,y){
+    drawTile((x-player.x+offset), (y-player.y+offset),level[x][y]);
 }
 
 function moveTo(clientId, x, y){
-	//get special client
-	var client;
-	for (var i=0; i<players.length; i++){
-		if (players[i].idNr === clientId) {
-			client = players[i];
-		}
-	}
-	console.log(client);
+    //get moved client
+    var client;
+    for (var i=0; i<players.length; i++){
+        if (players[i].idNr === clientId) {
+            client = players[i];
+        }
+    }
+    console.log(client);
 
-	level[client.x][client.y].player = false;
-	level[x][y].player = true;
 
-	//reset previous tile
-	if ((client.x >= player.x-1 && client.x <= player.x+1) && (client.y >= player.y-1 && client.y <= player.y+1)) {
-		console.log("prev pos visible: " + (client.x-player.x+1) + ":" + (client.y-player.y+1));
-		drawTile((client.x-player.x+1), (client.y-player.y+1),level[client.x][client.y]);
-	}
+    // move player count on level
+    level[client.x][client.y].players--;
+    level[x][y].players++;
 
-	//test if visible
-	if ((x >= player.x-1 && x <= player.x+1) && (y >= player.y-1 && y <= player.y+1)) {
-		console.log("client visible!");
+    // reset previous tile if visible
+    if (isVisible(client.x, client.y)) {
+        console.log("prev pos visible: " + (client.x-player.x+offset) + ":" + (client.y-player.y+offset));
+        drawTile((client.x-player.x+offset), (client.y-player.y+offset),level[client.x][client.y]);
+    }
 
-		console.log("drawing player: x=" + (x-player.x+1) *tileWidth + " y=" + (y-player.y-1) *tileWidth);
-		ctx.drawImage(img, (x-player.x+1) *tileWidth, (y-player.y+1) *tileWidth, 100, 100);
+    // print client if visible
+    if (isVisible(x, y)) {
+        console.log("client visible!");
 
-	}
+        console.log("drawing player: x=" + (x-player.x+offset) *tileWidth + " y=" + (y-player.y-offset) *tileWidth);
+        ctx.drawImage(img, (x-player.x+offset) *tileWidth, (y-player.y+offset) *tileWidth, 100, 100);
 
-	client.x=x;
-	client.y=y;
+    }
+
+    // set new client position
+    client.x=x;
+    client.y=y;
+
+    //draw own player
+    ctx.drawImage(img,offset*tileWidth,offset*tileWidth, tileWidth, tileWidth);
+}
+
+function isVisible(x,y){
+    return ((x >= player.x-offset && x <= player.x+offset) && (y >= player.y-offset && y <= player.y+offset));
 }
 
 function drawView(){
-	console.log("drawing view");
-	/*
-	for(var i=player.x-1; i<=player.x+1; i++){
-		for(var j=player.y-1; j<=player.y+1; j++){
-			drawTile(i,j);
-		}
-	}*/
+    // redraw whole view port
+    ctx.clearRect(0, 0, viewWidth*tileWidth, viewWidth*tileWidth);
+    for(var i=0; i<viewWidth; i++){
+        for(var j=0; j<viewWidth; j++){
+            var y = player.y-tileWidth;
+            var x = player.x-tileWidth;
+            if (level[player.x-offset+i] && level[player.x-offset+i][player.y-offset+j])
+                drawTile(i,j,level[player.x-offset+i][player.y-offset+j]);
+        }
+    }
 
-	ctx.clearRect(0, 0, 3*tileWidth, 3*tileWidth);
-	for(var i=0; i<3; i++){
-		for(var j=0; j<3; j++){
-			var y = player.y-tileWidth;
-			var x = player.x-tileWidth;
-			drawTile(i,j,level[player.x-1+i][player.y-1+j]);
-		}
-	}
-
-	//ctx.drawImage(img,1*tileWidth,1*tileWidth,tileWidth, tileWidth);
+    //draw own player
+    //ctx.drawImage(img,1*tileWidth,1*tileWidth, tileWidth, tileWidth);
 }
 
 function drawTile(x, y, tile){
-	//console.log("x: " + x + " y: " + y + " tile: " + tile);
-	if (tile.background === "wall") {
-		ctx.drawImage(wall, x*tileWidth, y*tileWidth);
-	} else {
-		ctx.drawImage(floor, x*tileWidth, y*tileWidth);
-	}
-	if (tile.player){
-		ctx.drawImage(img, x*tileWidth, y*tileWidth, 100, 100);
-	}
+    // draw a single tile
+    // 
+    //console.log("x: " + x + " y: " + y + " tile: " + tile);
+    if (tile.background === "wall") {
+        ctx.drawImage(wall, x*tileWidth, y*tileWidth);
+    } else {
+        ctx.drawImage(floor, x*tileWidth, y*tileWidth);
+    }
+
+    //draw other players
+    //console.log(tile.players);
+    if (tile.players > 0){
+        console.log("drawing other player");
+        ctx.drawImage(img, x*tileWidth, y*tileWidth, 100, 100);
+    }
 }
 
 function generateLevel(){
-	console.log("generate level");
-	var l = [];
-	for (var i=0; i<levelWidth; i++){
-		l[i] = [];
-		for (var j=0; j<levelHeight; j++){
-			if (i === 0 || j === 0 || i === levelWidth-1 || j === levelWidth-1){
-				l[i][j] = {background: "wall"};
-			} else {
-				l[i][j] = {background: "floor"};
-			}
-		}
-	}
-	console.log(l);
-	
-	return l;
+    
+    console.log("generate level");
+    var l = [];
+    for (var i=0; i<levelWidth; i++){
+        l[i] = [];
+        for (var j=0; j<levelHeight; j++){
+            if (i === 0 || j === 0 || i === levelWidth-1 || j === levelWidth-1){
+                l[i][j] = {background: "wall"};
+            } else {
+                l[i][j] = {background: "floor"};
+            }
+            l[i][j].players = 0;
+        }
+    }
+    console.log(l);
+    
+    return l;
 }
 
 function emitNewPosition(){
-	socket.emit('move',{
-		x: player.x,
-		y: player.y,
-		idNr: player.idNr
-	});
+    socket.emit('move',{
+        x: player.x,
+        y: player.y,
+        idNr: player.idNr
+    });
 }
 
 });
